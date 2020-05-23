4b7aff3f640c604a79e63e62e04706f52da32947
tana3n <tana3n@gmail.com>
2020-05-23 22:24:00 +0900

    First Release
    とりあえず動くようになった

---
+++ event_page.js
@@ -0,0 +1,31 @@
+  //コンテキスト表示
+  chrome.contextMenus.create({
+    'id' : "mkcM",
+    'title' : 'Nijie Download',
+    'type' : 'normal',
+    "contexts" : ["page"],
+    "documentUrlPatterns" : ["*://nijie.info/view.php?id=*"]
+    /*,
+    'onclick' : function(info){
+      chrome.extention.sendMessage({type: 'get'});
+    }*/
+  });
+//選択時のイベント
+chrome.contextMenus.onClicked.addListener(function (info,tab) {
+  console.log('sendMessage')
+ // if (info.menuItemId === 'nkcM'){
+  chrome.tabs.query( {active:true, currentWindow:true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id,{message: 'getImage'})
+  console.log('sentMessage')
+});
+  
+  //}
+});
+
+//とりあえずこれで受けられてるのでこのままで
+chrome.extension.onMessage.addListener(function(request) {
+    chrome.downloads.download({
+      url: request.url,
+      filename: request.filename
+    });
+});
---
+++ nijie-download.js
@@ -0,0 +1,144 @@
+  'use strict'; 
+  var checkLogin, getSrcURL,　getExttype,　getDiff, getFile, BT, dl;
+  //macro
+  var getTitle, getIllustID, getFilename, getDiffmacro, getUsername, getUserID;
+  
+  checkLogin = function(){
+   if($('#main>div').attr("class") == 'login'){
+    return false
+   } else {
+    return true
+   }
+  }
+  getSrcURL = function(diff) {
+  //一枚目
+    if(diff == 0){    
+      var imgfilter = $('#img_filter img').attr("src");
+      if (imgfilter.length > 0) {
+        return imgfilter;
+      } else {
+        console.log('cant get SrcURL');  
+        return null;
+      }
+    }else if(diff > 0){
+      var imgfilter = $('#img_diff img').eq(diff-1).attr("src");
+      imgfilter　= imgfilter.replace('__rs_l120x120/','');
+      return imgfilter;
+    } else {
+      console.log('no detect img_diff')
+    }
+  };
+
+  getDiff =　function() {
+    var diff = $('#img_diff a:last').attr("href");
+    if (diff == null){
+      return diff = 0
+    }else{
+      diff = diff.replace(/[0-9]+/,'');
+      return diff.replace('./view_popup.php?id=&#diff_','');
+    }
+  }
+    
+//img_diff内に中身があるかどうかで判定かなぁ
+//最後のdiff_*を引っ張り出して枚数検出
+    getDiffmacro　=　function(num){
+      return (''+num).padStart(2,'0');
+    }
+    //0パテ用
+    getIllustID = function(){
+      return $('#img_filter img').attr('illust_id');
+    };
+    getUserID = function(){
+      return $('#img_filter img').attr('user_id');
+    };
+    getTitle = function(){
+      return $('#img_filter img').attr('alt');
+    };
+    getUsername = function(){
+      return $('#pro img').attr('alt');
+    } 
+    getExttype = function(url){
+      return url.match(/\.[^.]+$/);
+    }
+    // $('#ID名 ほげほげ')でaとかliとかの子要素がとれる（空白だとJqueryだと孫要素いけます ＞だと子だけなので明示したいときはそっちかな
+//attr('ほげ');で(classとかhrefとか)の内容が拾える
+
+
+getFilename　= function(num){
+//とりあえずマクロ化するはずだけどとりあえず仮処理
+//macro
+var Exttype = getExttype(getSrcURL(getDiff()));
+//ExtTypeは必須なのでなんかうまーくreturnにねじ込む
+var Pagenum2 = getDiffmacro(num)
+var Pagenum = num
+var Maxpagenum2 = getDiffmacro(getDiff())
+var Maxpagenum = getDiff()
+
+var basename = getUsername()+String('(')+getUserID()+String(')')+String(' - ')+getTitle()+String('(')+getIllustID()+String(')')
+var diffmacro = Pagenum2+String('of')+Maxpagenum2
+// basename+diffmacroにするかbasename2作るかは要検討
+if(getDiff() == 0){
+return　basename+Exttype;
+}else{
+return　basename+diffmacro+Exttype;
+
+}
+//とりあえずデフォはこれで、あとはタグに任意のやつが含まれてたらフォルダ分けぐらいはしたいなぁ（Ex:VOICEROID)
+}
+
+
+//dlボタンはしばらく忘れよう
+/*
+BT = function(){
+  var img = $('<img>');
+//  view-center-button>');
+  //imgタグに追加する奴
+  img.attr('src', chrome.extension.getURL('icon_32.png'));
+  img.attr('draggable', false);
+  var a = $('<a>');
+  a.attr('href', 'javascript:void(0);');
+    var main = function() {
+    if(checkLogin() == true){
+    a.one('click',dl());   
+    }else{
+    alert("ログイン状態を確認できませんでした");
+    }
+    div = $('<div>');
+    div.attr('id', 'SD');
+    div.append(a);
+    var parent = $('.thum_large').eq(0);
+    return parent.prepend(div);
+  };
+}*/
+  dl = function(){
+    var diff = getDiff();
+    for(var num = 0; num <= diff ; num++){
+      var filename = getFilename(num);
+      var url = 'https:'+getSrcURL(num);
+      console.log(filename);
+      console.log(url);
+      getFile(url,filename);
+  }
+  }
+
+  getFile = function(url, filename) {
+    return chrome.extension.sendMessage({
+     type: 'download',
+     url: url,
+     filename: filename
+    });
+  };
+  chrome.runtime.onMessage.addListener(function(request,sender){
+console.log('login:'+checkLogin());
+if (checkLogin() == true){
+  //OK
+  console.log("GetPageInfomation")
+  console.log('Username:'+getUsername());  
+  console.log('user_id:'+getUserID());  
+  console.log('Title:'+getTitle());  
+  console.log('illust_id:'+getIllustID());  
+  console.log('差分数:'+getDiffmacro(getDiff()));  
+  dl();
+
+}
+});

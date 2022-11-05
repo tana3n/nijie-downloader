86e527c54588cd1e69cfb4f137cd9e1792fc1c0e
tana3n <tana3n@gmail.com>
2022-11-05 13:34:11 +0900

    nijie-downloader: Refactoring and fix extension resolver 書き方の変更と拡張子が常に一番最後のファイルの拡張子にあわせられる不具合を修正

--- nijie-download.js
+++ nijie-download.js
@@ -3,97 +3,95 @@
   //macro
   var getTagnum,getTags,getTag, getTitle, getIllustID, getFilename, getDiffmacro, getUsername, getUserID;
   
-  checkLogin = function(){
-   
-    if(document.getElementsByClassName('login').length == 1){
-      return false;
-      } else {
-      return true;
-      }
+function checkLogin() {
+  if ( document.getElementsByClassName('login').length == 1 ){
+    return false;
+  } else {
+    return true;
+  }
+}
+function getSrcURL(diff) {
+  if(diff == 0){//一枚目
+    try{
+      return document.getElementById('img_filter').querySelector('img').src;
+    } catch {
+      return document.getElementById('img_filter').querySelector('video').src;
     }
-  getSrcURL = function(diff) {
-  //一枚目
-    if(diff == 0){    
-      try{
-          return document.getElementById('img_filter').querySelector('img').src;
-      } catch {
-         return document.getElementById('img_filter').querySelector('video').src;
-      }
-      } else if (diff > 0){
-      var imgfilter =document.getElementById('img_diff').querySelectorAll('img')[diff-1].src;
-      imgfilter　= imgfilter.replace('__rs_l120x120/','');
-      return imgfilter;
-    } else {
-      console.log('no detect img_diff')
-    }
-  };
+  } else if (diff > 0) {
+    var imgfilter =document.getElementById('img_diff').querySelectorAll('img')[diff-1].src;
+    imgfilter　= imgfilter.replace('__rs_l120x120/','');
+    return imgfilter;
+  } else {
+    console.log('no detect img_diff')
+  }
+};
 
-  getDiff =　function() {
-    var diff = document.getElementById('img_diff').querySelectorAll('img').length;
-    if (diff == null){
-      return 0
-    }else{
-      return diff;
-    }
+function getDiff() {
+  var diff = document.getElementById('img_diff').querySelectorAll('img').length;
+  if (diff == null){
+    return 0
+  } else {
+    return diff;
   }
-  getTag = function(Tagnum){
-    var e = document.getElementById('view-tag').querySelectorAll('.tag')[Tagnum].innerText;
-    //replace Taglock
-    return e.replace('*','');
-  }
+}
+
+function getTag(Tagnum){
+  var e = document.getElementById('view-tag').querySelectorAll('.tag')[Tagnum].innerText;
+  return e.replace('*','');//replace Taglock
+}
   //タグ数取得
-  getTagnum = function(){
-    return document.getElementById('view-tag').querySelectorAll('.tag').length;
-  }
-  getTags = function(){
-    var Tagnum = getTagnum();
-    console.log(Tagnum)
-    for(var num = 0; num < Tagnum ; num++){
+function getTagnum(){
+  return document.getElementById('view-tag').querySelectorAll('.tag').length;
+}
+
+function getTags(){
+  var Tagnum = getTagnum();
+  for(var num = 0; num < Tagnum ; num++){
      console.log(getTag(num))
-    }
   }
+}
 //img_diff内に中身があるかどうかで判定かなぁ
 //最後のdiff_*を引っ張り出して枚数検出
-    getDiffmacro　=　function(num){
-      return (''+(num+1)).padStart(2,'0');
-    }//0パテ用
-    
-    getIllustID = function(){
-      try{
-        return document.getElementById('img_filter').querySelector('img').getAttribute('illust_id');
-      } catch {
-        return  document.getElementById('img_filter').querySelector('video').getAttribute('illust_id');
-      }
-    };
-    getUserID = function(){
-      try{
-        return document.getElementById('img_filter').querySelector('img').getAttribute('user_id');
-      } catch {
-        return  document.getElementById('img_filter').querySelector('video').getAttribute('user_id');
-      }
-    };
-    getTitle = function(){
-      try{
-        return document.getElementById('img_filter').querySelector('img').getAttribute('alt');
-      } catch {
-        return document.getElementById('img_filter').querySelector('video').getAttribute('alt');
-      }
-    };
-    getUsername = function(){
-      return document.getElementById('pro').querySelector('img').alt;
-    };
-    getExttype = function(url){
-      return url.match(/\.[^.]+$/);
-    };
-    // $('#ID名 ほげほげ')でaとかliとかの子要素がとれる（空白だとJqueryだと孫要素いけます ＞だと子だけなので明示したいときはそっちかな
-//attr('ほげ');で(classとかhrefとか)の内容が拾える
+function getDiffmacro(num){
+  return (''+(num+1)).padStart(2,'0');
+}//0パテ用
 
+//この3つは統合できそう&img videoは初手で判定させた方がきれいなので保留
+getIllustID = function(){
+  try{
+    return document.getElementById('img_filter').querySelector('img').getAttribute('illust_id');
+  } catch {
+    return  document.getElementById('img_filter').querySelector('video').getAttribute('illust_id');
+  }
+};
+getUserID = function(){
+  try{
+    return document.getElementById('img_filter').querySelector('img').getAttribute('user_id');
+  } catch {
+    return  document.getElementById('img_filter').querySelector('video').getAttribute('user_id');
+  }
+};
+getTitle = function(){
+  try{
+    return document.getElementById('img_filter').querySelector('img').getAttribute('alt');
+  } catch {
+    return document.getElementById('img_filter').querySelector('video').getAttribute('alt');
+  }
+};
+    
+getUsername = function(){
+  return document.getElementById('pro').querySelector('img').alt;
+};
+
+getExttype = function(url){
+  return url.match(/\.[^.]+$/);
+};
 
 getFilename　= function(num){
   //とりあえずマクロ化するはずだけどとりあえず仮処理
   //macro
-  console.log(getSrcURL(getDiff()))
-  var Exttype = getExttype(getSrcURL(getDiff()));
+  console.log(getSrcURL(num))
+  var Exttype = getExttype(getSrcURL(num));
   var Pagenum2 = getDiffmacro(num)
   var Maxpagenum2 = getDiffmacro(getDiff())
   var Maxpagenum = getDiff()
@@ -151,7 +149,7 @@ BT = function(){
      filename: filename
     });
   };
-
+//せめてDebugフラグ立ってたらとか、ね？
   console.log('for Debug');
   console.log('get TagNum:'+getTagnum());
   console.log('get All Tag');
